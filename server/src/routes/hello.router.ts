6ed6dc3ada41df1dccaea01d5417e12961f80fd7
p1803622 <p1803622>
2025-01-07 15:08:50 +0100

    second commit

---
+++ server/src/routes/hello.router.ts
@@ -0,0 +1,24 @@
+import express from 'express';
+
+const helloRouteur = express.Router();
+
+// With middlewares you can ensure the user is authenticated
+// before requesting secured API routes
+helloRouteur.use((request, response, next) => {
+  const requestIP = request.ip ?? 'unknown';
+  process.stdout.write('HelloRouter Middleware\n');
+  if (requestIP.endsWith('127.0.0.1')) {
+    process.stdout.write('Request from local IP\n');
+    next();
+  } else {
+    next();
+  }
+});
+
+helloRouteur.get('/', (request, response) => {
+  response.send('Hello TIW8 !');
+});
+
+export {
+  helloRouteur as HelloRouteur
+};
